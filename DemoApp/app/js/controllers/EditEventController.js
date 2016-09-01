952707063c9a8354f41781c684fefedc63e64ad4
IsYet1 <Don@IsYet.com>
2016-09-01 14:18:20 -0500

    Validation pattern is working

--- DemoApp/app/js/controllers/EditEventController.js
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -2,8 +2,13 @@
 
 eventsApp.controller('EditEventController', 
     function EventController($scope) {
-        $scope.saveEvent = function(event){
-            alert(event.name);
+        $scope.saveEvent = function(event, newEventForm){
+            if(newEventForm.$valid){
+                alert(event.name);
+            }
+            else{
+                alert("Form is not valid");
+            }
         };
 
         $scope.cancelEdit = function(){
