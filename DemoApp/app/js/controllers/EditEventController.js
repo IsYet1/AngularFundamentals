acaa23d87ca11551724d222737f09e444e8da69a
IsYet1 <Don@IsYet.com>
2016-09-01 14:33:03 -0500

    Validation is working. Added classes. Disabling the button on invalid form.

--- DemoApp/app/js/controllers/EditEventController.js
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -3,6 +3,7 @@
 eventsApp.controller('EditEventController', 
     function EventController($scope) {
         $scope.saveEvent = function(event, newEventForm){
+            console.log(newEventForm);
             if(newEventForm.$valid){
                 alert(event.name);
             }
